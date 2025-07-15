7b976b437ec13cd02962a525969e819e3c759400
Tobias Tefke <tobias@tefke.name>
2025-07-15 16:43:45 +0200

    Add ability to authenticate traefik forwardauth requests for noVNC

--- authenticate.go
+++ authenticate.go
@@ -5,17 +5,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/kataras/jwt"
 )
 
+var lastJWTAuthentication time.Time
+
 func authenticate(w http.ResponseWriter, r *http.Request) {
 	/* Get parameters from URL */
-	tokenString := r.URL.Query().Get("token")
-	service := r.URL.Query().Get("service")
-	result := AuthenticationResponse{false}
+	var tokenString string
+	var service string
 
-	log.Println("Received request: ", r.URL)
+	// Figure out if this is an authentication request from Traefik
+	forwardedURI := r.Header.Get("X-Forwarded-Uri")
+	if forwardedURI != "" {
+		// Traefik request
+		log.Println("Received forwarded URI request: ", forwardedURI)
+
+		// Check if within time frame of 15 seconds (assume resources are loaded)
+		if time.Since(lastJWTAuthentication).Seconds() < 15 {
+			http.Error(w, "Success", 200)
+			return
+		}
+
+		// Parse URI
+		parsedURI, err := url.Parse(forwardedURI)
+		if err != nil {
+			// Can not parse URI -> Invalid
+			log.Println("Can not parse URI: ", forwardedURI)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Get token and service strings from X-Forwarded-Uri header
+		tokenString = parsedURI.Query().Get("token")
+		service = parsedURI.Query().Get("service")
+	} else {
+		// Get token and service strings from URL
+		log.Println("Received request: ", r.URL)
+		tokenString = r.URL.Query().Get("token")
+		service = r.URL.Query().Get("service")
+	}
+
+	// Prepare response
+	result := AuthenticationResponse{false}
 
 	/* Check if token and service are present */
 	if tokenString != "" && service != "" {
@@ -32,6 +67,11 @@ func authenticate(w http.ResponseWriter, r *http.Request) {
 				log.Println(err)
 			} else {
 				result.Allowed = decide(service, claims)
+
+				// Update time
+				if result.Allowed && service == "noVNC" && forwardedURI != "" {
+					lastJWTAuthentication = time.Now()
+				}
 			}
 		}
 	} else {
